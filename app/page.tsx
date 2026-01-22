03dcc22f7ecf63284cb1e664b87f5b86babc7c40
homebidnow-del <homebidnow@gmail.com>
2026-01-22 14:57:34 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>RenoBids</h1>
+      <p>Post your renovation. Get multiple estimates.</p>
+    </main>
+  );
+}
